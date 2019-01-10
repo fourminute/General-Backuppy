3ac7dd267859ce3339b65542bdf9a3f371d8a644
fourminute <38321299+fourminute@users.noreply.github.com>
2019-01-10 11:07:45 -0800

    Update BackUpDatFilesYo.py

--- BackUpDatFilesYo.py
+++ BackUpDatFilesYo.py
@@ -4,8 +4,19 @@
 from shutil import copyfile
 import hashlib
 import os
-sources = []
-mirrors = []
+
+try:#Python2
+    import Tkinter as tk 
+    from Tkinter import filedialog
+    import tkMessageBox
+    from Tkinter import *
+except:#Python3
+    import tkinter as tk 
+    from tkinter import filedialog
+    from tkinter import messagebox
+    from tkinter import *
+source = ""
+mirror = ""
 hashtype = "MD5"
 fileshashed = 0
 filescopied = 0
@@ -19,36 +30,37 @@ def inccopied():
 def incfailed():
   global filesfailed
   filesfailed += 1
-  
-try:#Python2
-    import Tkinter as tk 
-    from Tkinter import filedialog
-    import tkMessageBox
-    from Tkinter import *
-except:#Python3
-    import tkinter as tk 
-    from tkinter import filedialog
-    from tkinter import messagebox
-    from tkinter import *
+def changelabel(text):
+    labeltxt.set(text)
 
-#Ask for Directory
 root = Tk()
-root.withdraw()
-srcdir = filedialog.askdirectory(title='Please select a source directory.')
-mrrdir = filedialog.askdirectory(title='Please select a mirror directory.')
+root.resizable(0,0)
+labeltxt = tk.StringVar()
+winlabel = Label(root, textvariable=labeltxt)
+winlabel.pack(padx=20,pady=10)
+def quit():
+    try:#Python2
+        result = tkMessageBox.askyesno("Are you sure?","Cancel operation and exit?")
+    except:#Python3
+        result = messagebox.askyesno("Are you sure?","Cancel operation and exit?")
+    if result == True:
+        exit()
+root.protocol("WM_DELETE_WINDOW",quit)
+changelabel("Awaiting File Selection.")
+source = filedialog.askdirectory(title='Please select a source directory.')
+mirror = filedialog.askdirectory(title='Please select a mirror directory.')
+sourcefiles = [os.path.join(r,file) for r,d,f in os.walk(source) for file in f]
+mirrorfiles = [os.path.join(r,file) for r,d,f in os.walk(mirror) for file in f]
 result = ""
+changelabel("Files Selected.")
 try:#Python2
     result = tkMessageBox.askyesno("Confirm","Proceed with copy operation?")
 except:#Python3
     result = messagebox.askyesno("Confirm","Proceed with copy operation?")
 
 
-if result == True:
-    sources.append(srcdir)
-    mirrors.append(mrrdir)
-else:
+if not result == True:
     exit()
-
 
 
 #Simple MD5SUM
@@ -61,24 +73,30 @@ def md5sum(filename, blocksize=65536):
 
 
 # Mirror files operation
-def mirror(source, mirror, index):
-    for st in source:
+def start():
+    for st in sourcefiles:
         file = os.path.basename(st)
         directory = os.path.split(st)[0]
-        newfilepath = st.replace(sources[index], mirrors[index])
+        newfilepath = st.replace(source, mirror)
         newdirectory = os.path.split(newfilepath)[0]
+        fncount = sum([len(files) for r, d, files in os.walk(source)])
+        #Checking if directory exists
+        if not os.path.exists(newdirectory):
+            print("Directory " + "'" + os.path.basename(newdirectory) + "'" + " doesn't exist, creating.")
+            os.makedirs(newdirectory)
+            print("Created directory " + "'" + os.path.dirname(newfilepath) + "'")
+                
+        #Copy operation
         if not os.path.exists(newfilepath):
-                if not os.path.exists(newdirectory):
-                    print("Directory " + "'" + os.path.basename(newdirectory) + "'" + " doesn't exist, creating.")
-                    os.makedirs(newdirectory)
-                    print("Created directory " + os.path.dirname(newfilepath))    
-                    try:
-                        copyfile(st, newfilepath)
-                        inccopied()
-                        print("Copied file " + "'" +file + "'" + " > \\" + os.path.basename(newdirectory) + "\\" + file)
-                    except:
-                        print("Copy operation failed, unknown error.")
-                        incfailed()
+            try:
+                copyfile(st, newfilepath)
+                inccopied()
+                print("Copied " + "'" +file + "'" + " to " + "'" + ".../" + os.path.basename(newdirectory) + "/" + file + "'"+ ".")
+                changelabel("Copying Files\nProgress: " + str(filescopied + fileshashed) + " of " + str(fncount) + ".")
+                root.update()
+            except:
+                print("Copy operation failed, unknown error.")
+                incfailed()
                 
         else:
             srchash = md5sum(st)
@@ -87,24 +105,22 @@ def mirror(source, mirror, index):
             if srchash == mirrorhash:
                 print("File " + "'" + file + "'" + " exists. Matching " + hashtype + "SUM!")
             else:
-                print("File " + "'" + file + "'" + " exists, hash didn't match. Overwriting file > " + os.path.dirname(newfilepath))
+                print("File " + "'" + file + "'" + " exists, hash didn't match. Overwriting to " + "'" + os.path.dirname(newfilepath) + "'"+ ".")
                 try:
                     copyfile(st, newfilepath)
                     inccopied()
-                    print("Copied file " + "'" +file + "'" + " > \\" + os.path.basename(newdirectory) + "\\" + file)
+                    print("Copied " + "'" +file + "'" + " to " + "'" + ".../" + os.path.basename(newdirectory) + "/" + file + "'" + ".")
+                    changelabel("Copying Files\nProgress: " + str(filescopied + fileshashed) + " of " + str(fncount) + ".")
                 except:
-                    print("Copy operation failed, unknown error.")
+                    print("Failed to copy " + "'" + file + "'" + ".")
                     incfailed()
                     
 
 
-index = 0
-size = len(sources) 
-while index < size :
-    sourcefiles = [os.path.join(r,file) for r,d,f in os.walk(sources[index]) for file in f]
-    mirrorfiles = [os.path.join(r,file) for r,d,f in os.walk(mirrors[index]) for file in f]
-    mirror(sourcefiles, mirrorfiles, index)
-    index += 1
 
+start()
 
-messagebox.showinfo("Operation Completed","Total Files Copied: " + str(filescopied) + ". \nTotal Files Hashed: " + str(fileshashed) + ".\nFailed To Copy: " + str(filesfailed) + ".")
+try:#Python3
+    messagebox.showinfo("Operation Completed","Total Files Copied: " + str(filescopied) + ". \nTotal Files Hashed: " + str(fileshashed) + ".\nFailed To Copy: " + str(filesfailed) + ".")
+except:
+    tkMessageBox.showinfo("Operation Completed","Total Files Copied: " + str(filescopied) + ". \nTotal Files Hashed: " + str(fileshashed) + ".\nFailed To Copy: " + str(filesfailed) + ".")
